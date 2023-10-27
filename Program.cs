fb828915a61e0f22c447665861c51e41f8ac903b
Ahmed Zeb <ahmed.z@pacsquare.com>
2023-10-27 15:50:29 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace FacadeDesignPattern
+{
+    internal class Program
+    {
+        // Facade Design Pattern is used to hide the complexities of a system and provides
+        // an easy-to-use interface to the client using which the client can access the
+        // system. The Facade (usually a wrapper) class sits on the top of a group of
+        // subsystems and allows them to communicate in a unified manner
+        static void Main(string[] args)
+        {
+            Order order = new Order();
+            order.PlaceOrder();
+            Console.Read();
+        }
+    }
+}
